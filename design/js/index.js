9b00346cb0afdbbf47f54c853199f05efe90b02a
Aly_Heshmat <bekali811@gmail.com>
2023-07-10 12:35:40 +0300

    the main commit

---
+++ design/js/index.js
@@ -0,0 +1,47 @@
+let charts = document.querySelectorAll('#chart');
+let chartSpans = document.querySelectorAll('#chart span');
+
+
+fetch('../../data.json').then(res => {
+    return res.json();
+}).then(data => {
+
+    for (let i = 0; i < data.length; i++) {
+
+        charts[i].className = data[i].day
+        charts.forEach(chart => {
+            if (data[i].day === chart.className) {
+                chart.dataset.data = data[i].amount
+            }
+
+            chartHeght = `${chart.dataset.data * 3}px`
+
+            chart.style = `height: ${chartHeght}`
+        })
+    }
+}).then(data => {
+    for (let i = 0; i < charts.length; i++) {
+
+        chartSpans[i].textContent = `$${charts[i].dataset.data}`;
+    }
+
+    charts.forEach(chart => {
+
+        let day = new Date().getDay()
+        let arr = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+    
+        let dayWeek = arr[day];
+    
+        if (chart.className === dayWeek) {
+            chart.classList.add('current');
+        }
+    
+    })
+})
+
+
+
+
+
+console.log(day)
+console.log(dayWeek);
